6b8e88c7fa554fec95bd29f18313e588aab7a749
Valerij Medviď <vmedvid@gmail.com>
2022-02-16 19:09:19 +0100

    git init

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup></script>
+
+<template>BOMB FINDER</template>
+
+<style lang="scss">
+@import "./assets/css/base.scss";
+
+#app {
+  max-width: 1280px;
+  margin: 0 auto;
+  padding: 2rem;
+
+  font-weight: normal;
+}
+</style>
